2c0c624e664fa70a3974e8c3d0b2a2654eab11dd
Aditya Sinha <Aditya.sinha1990@outlook.com>
2022-01-31 04:03:08 +1100

    Delete Dir app2
    Delete Dir app1

--- app2_files/static/javascript_files/redirecting.js
+++
@@ -1,7 +0,0 @@
-function myBlogSite() {
-  window.location.href = "https://medium.com/@mahendraditya.sinha/python-list-comprehensions-data-structure-comprehension-part-1-9f03568cc494";
-}
-function calling_redirecting_js(){
-    alert("Redirecting to my Blog")
-    myBlogSite()
-}
--- app2_files/static/javascript_files/serverinfo.js
+++
@@ -1,7 +0,0 @@
-function server_host() {
-  return document.getElementById("adi_name_text").innerHTML = window.location.host;
-}
-
-function server_filepath() {
-  return document.getElementById("adi_name_text").innerHTML = window.location.href;
-}
